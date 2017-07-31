5e3ae5a91811d4c009bea3bba6a2d3d95dda7771
rodrigap <rodrigap@whitman.edu>
2017-07-31 16:57:24 -0700

    ngrok

--- kittenbot.js
+++ kittenbot.js
@@ -1,38 +1,5 @@
-/* *****************************************************************************
-Copyright 2016 Google Inc. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License")
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-********************************************************************************
-
-This is a sample Slack bot built with Botkit.
-*/
-//
-// var Botkit = require('botkit')
-//
-// var controller = Botkit.slackbot({debug: false})
-// controller
-//   .spawn({
-//     token: 'xoxb-221043661830-R90Ku1gIH8ZX106fWMQ1wOv0' // Edit this line!
-//   })
-//   .startRTM(function (err) {
-//     if (err) {
-//       throw new Error(err)
-//     }
-//   })
-//
-// controller.hears(
-//   ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-//   function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
+//IF NGROK GETS RESET, PUT NEW FORWARD URL HERE:
+//https://api.slack.com/apps/A6G2BGPUK/interactive-messages?saved=1
 
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
@@ -44,7 +11,8 @@ rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message);
-  rtm.sendMessage('HEY ITS KAMRAN', message.channel)
+  rtm.sendMessage('HEY ITS KAMRAN', message.channel);
+
 });
 
 
