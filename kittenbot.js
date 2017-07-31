de66ff2ad73c2c0da6842e16e64a05a5cbb20ccb
rodrigap <rodrigap@whitman.edu>
2017-07-31 14:48:11 -0700

    sample bot code

--- kittenbot.js
+++ kittenbot.js
@@ -16,20 +16,41 @@ limitations under the License.
 
 This is a sample Slack bot built with Botkit.
 */
+//
+// var Botkit = require('botkit')
+//
+// var controller = Botkit.slackbot({debug: false})
+// controller
+//   .spawn({
+//     token: 'xoxb-221043661830-R90Ku1gIH8ZX106fWMQ1wOv0' // Edit this line!
+//   })
+//   .startRTM(function (err) {
+//     if (err) {
+//       throw new Error(err)
+//     }
+//   })
+//
+// controller.hears(
+//   ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
+//   function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
 
-var Botkit = require('botkit')
+var RtmClient = require('@slack/client').RtmClient;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var controller = Botkit.slackbot({debug: false})
-controller
-  .spawn({
-    token: 'xoxb-221043661830-R90Ku1gIH8ZX106fWMQ1wOv0' // Edit this line!
-  })
-  .startRTM(function (err) {
-    if (err) {
-      throw new Error(err)
-    }
-  })
+var token = process.env.SLACK_API_TOKEN || '';
+// var token = 'xoxb-221043661830-xwLcsxcgSVHVM4fC96V5Veyb';
 
-controller.hears(
-  ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-  function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
+var rtm = new RtmClient(token, { logLevel: 'debug' });
+rtm.start();
+
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  console.log('Message:', message);
+});
+
+rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+  console.log('Reaction added:', reaction);
+});
+
+rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+  console.log('Reaction removed:', reaction);
+});
