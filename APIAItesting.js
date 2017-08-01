abf84b1e06b6cadc88df60cb0d7058f64bbf28f8
Johnathan Zhou <johnathanz@berkeley.edu>
2017-08-01 15:15:12 -0700

    testing

--- APIAItesting.js
+++ APIAItesting.js
@@ -1,5 +1,19 @@
-//IF NGROK GETS RESET, PUT NEW FORWARD URL HERE:
-//https://api.slack.com/apps/A6G2BGPUK/interactive-messages?saved=1
+
+
+
+
+// // rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+// //   console.log('Reaction added:', reaction);
+// // });
+// //
+// // rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+// //   console.log('Reaction removed:', reaction);
+// // });
+//
+// var port = process.env.PORT || 3000;
+// app.listen(port);
+// console.log('Express started. Listening on port %s', port);
+
 
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
@@ -11,7 +25,10 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var connect = process.env.MONGODB_URI;
-var axios = require('axios')
+var axios = require('axios');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var models = require('./models/models');
 var routes = require('./routes/routes');
 
@@ -41,73 +58,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-//   console.log('Message:', message);
-  // if (message.subtype === 'bot_message') {
-  //   return;
-  // }
-  // else {
-//     web.chat.postMessage(message.channel, 'yo', { "attachments": [
-//           {
-//               "fallback": "You are unable to choose a game",
-//               "callback_id": "wopr_game",
-//               "color": "#3AA3E3",
-//               "attachment_type": "default",
-//               "actions": [
-//                   {
-//                       "name": "game",
-//                       "text": "Chess",
-//                       "type": "button",
-//                       "value": "chess"
-//                   },
-//                   {
-//                       "name": "game",
-//                       "text": "Falken's Maze",
-//                       "type": "button",
-//                       "value": "maze"
-//                   },
-//                   {
-//                       "name": "game",
-//                       "text": "Thermonuclear War",
-//                       "style": "danger",
-//                       "type": "button",
-//                       "value": "war",
-//                       "confirm": {
-//                           "title": "Are you sure?",
-//                           "text": "Wouldn't you prefer a good game of chess?",
-//                           "ok_text": "Yes",
-//                           "dismiss_text": "No"
-//                       }
-//                   }
-//               ]
-//           }
-//       ]}, function(err, res) {
-//     if (err) {
-//       console.log('Error:', err);
-//     } else {
-//       console.log('Message sent: ', res);
-//     }
-//   });
-//   }
-// });
-
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  if (message.subtype === 'bot_message') {
-    return;
-  }
-  else {
-      postAI(message)
-      .then((data) =>
-        {
-        const msg = data.data.result.fulfillment.speech
-        console.log('THIS IS YOUR DATA: ', msg)
-        confirmMessage(message.channel, msg)
-      })
-      .catch((err) => (
-        console.log('error ', err)))
-  }
-})
-
 var postAI = function(message) {
   console.log('inside post request');
 
@@ -135,35 +85,97 @@ var confirmMessage = function(channel, message) {
               "color": "#3AA3E3",
               "attachment_type": "default",
               "actions": [
-                  {
-                      "name": "YesNo",
-                      "text": "Yes",
-                      "type": "button",
-                      "value": "yes"
-                  },
-                  {
-                      "name": "YesNo",
-                      "text": "No",
-                      "type": "button",
-                      "value": "no"
-                  }
-              ]
+                {
+                    "name": "reminder",
+                    "text": "Yes",
+                    "type": "button",
+                    "value": "scheduleReminder",
+                },
+                {
+                    "name": "reminder",
+                    "text": "No",
+                    "type": "button",
+                    "value": "dontScheduleReminder"
+                },
+            ]
           }
-      ]})
+      ]}, function(err, res) {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Message sent: ', res);
+
+      //USER INFO FOR MODEL:
+      var user = rtm.dataStore.getUserById(message.user);
+      console.log('HERES A BUNCH OF INFO: slackID: ', user.id, 'slackUsername: ', user.name, 'slackEmail: ', user.profile.email);
+    }
+    })
   }
   else {
     web.chat.postMessage(channel, message)
   }
 }
 
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+    var postOptions = {
+        uri: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+    request(postOptions, (error, response, body) => {
+        if (error){
+            console.log("CANT SENT RESPONSE YIKES")
+        }
+    })
+}
 
-// rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
-//   console.log('Reaction added:', reaction);
-// });
-//
-// rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
-//   console.log('Reaction removed:', reaction);
-// });
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  if (message.subtype === 'bot_message') {
+    return;
+  }
+  else {
+    var slackUsername = rtm.dataStore.getUserById(message.user);
+    if (slackUsername) {
+      // postAI(message)
+      // .then((data) =>
+      //   {
+      //     console.log('THIS IS THE TEST FUNCTION IT SHOULD BE BOOLEAN: ',test);
+      //   const msg = data.data.result.fulfillment.speech
+      //   console.log('THIS IS YOUR DATA: ', msg)
+      //   confirmMessage(message.channel, msg)
+      // })
+      // .catch((err) => (
+      //   console.log('error ', err)))
+        web.chat.postMessage(message.channel, 'please use this link '+process.env.WEBHOOK_URL+'/connect')
+    }
+    else {
+        web.chat.postMessage(message.channel, process.env.WEBHOOK_URL+'/auth')
+    }
+  }
+})
+const test = false;
+app.post('/interactive', urlencodedParser, (req, res) => {
+  var parsed = JSON.parse(req.body.payload);
+  var response = parsed.actions[0].value;
+  res.send(response)
+  test = response;
+
+  // var prev = req.body.payload.oriignal_message
+
+  //make calendar event here if response is yes
+})
+
+rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+  console.log('Reaction added:', reaction);
+});
+
+rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+
+  console.log('Reaction removed:', reaction);
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port);
