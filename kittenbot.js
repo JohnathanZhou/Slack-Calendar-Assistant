f8d6f67284a348f4d6ba74271f35f4813df58829
rodrigap <rodrigap@whitman.edu>
2017-07-31 17:58:33 -0700

    figuring out messages

--- kittenbot.js
+++ kittenbot.js
@@ -3,16 +3,62 @@
 
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var WebClient = require('@slack/client').WebClient;
 
 var token = process.env.SLACK_API_TOKEN || '';
+
+var web = new WebClient(token);
 
 var rtm = new RtmClient(token, { logLevel: 'debug' });
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   console.log('Message:', message);
-  rtm.sendMessage('HEY ITS KAMRAN', message.channel);
-
+  // rtm.sendMessage('HEY ITS KAMRAN', message.channel);
+  web.chat.postMessage(message.channel, 'HEY YOU', function (err, res) {
+    console.log('ok')
+  })
+//   web.chat.postMessage(message.channel, 'HEY YOU', [
+//         {
+//             "fallback": "You are unable to choose a game",
+//             "callback_id": "wopr_game",
+//             "color": "#3AA3E3",
+//             "attachment_type": "default",
+//             "actions": [
+//                 {
+//                     "name": "game",
+//                     "text": "Chess",
+//                     "type": "button",
+//                     "value": "chess"
+//                 },
+//                 {
+//                     "name": "game",
+//                     "text": "Falken's Maze",
+//                     "type": "button",
+//                     "value": "maze"
+//                 },
+//                 {
+//                     "name": "game",
+//                     "text": "Thermonuclear War",
+//                     "style": "danger",
+//                     "type": "button",
+//                     "value": "war",
+//                     "confirm": {
+//                         "title": "Are you sure?",
+//                         "text": "Wouldn't you prefer a good game of chess?",
+//                         "ok_text": "Yes",
+//                         "dismiss_text": "No"
+//                     }
+//                 }
+//             ]
+//         }
+//     ], function(err, res) {
+//   if (err) {
+//     console.log('Error:', err);
+//   } else {
+//     console.log('Message sent: ', res);
+//   }
+// });
 });
 
 
